4c08d64baefff7bafba483476d0b1893ef74f229
Yair Shimony <yairshsh@gmail.com>
2025-03-20 16:10:47 +0200

    fix figure for paper

--- src/analyze_results/analyze_results.py
+++ src/analyze_results/analyze_results.py
@@ -54,35 +54,48 @@ def plot_full(all_results_df):
 
 
 def plot_for_paper(all_results_df):
-    plt.rcParams.update({'font.size': 18})
-    fig, axs = plt.subplots(1, 3, figsize=(30, 16))
+    plt.rcParams.update({
+        "font.size": 22,  # Base font size
+        "axes.titlesize": 34,  # Title size
+        "axes.labelsize": 22,  # X & Y label size
+        "axes.labelweight": "bold",  # X & Y label bold
+        "xtick.labelsize": 22,  # X-axis tick size
+        "ytick.labelsize": 22,  # Y-axis tick size
+        # "font.weight": "bold"  # Global font weight
+    })
+    fig, axs = plt.subplots(1, 3, figsize=(30, 12))
 
-    sns.stripplot(data=all_results_df, x='model_id', y='test_mcc', hue='training_mode', ax=axs[0])
+    sns.stripplot(data=all_results_df, x='model_id', y='test_mcc', hue='training_mode', ax=axs[0], size=10)
     if ADD_BASELINES_TO_FIG:
         axs[0].axhline(y=0.81, color='r', linestyle='--', linewidth=1, label='y=0.81')
         axs[0].axhline(y=0.83, color='r', linestyle='--', linewidth=1, label='y=0.83')
 
-    sns.stripplot(data=all_results_df, x='model_id', y='test_auprc', hue='training_mode', ax=axs[1])
+    sns.stripplot(data=all_results_df, x='model_id', y='test_auprc', hue='training_mode', ax=axs[1], size=10)
     if ADD_BASELINES_TO_FIG:
         axs[1].axhline(y=0.88, color='r', linestyle='--', linewidth=1, label='y=0.88')
         axs[1].axhline(y=0.91, color='r', linestyle='--', linewidth=1, label='y=0.91')
 
-    sns.stripplot(data=all_results_df, x='model_id', y='test_elapsed_time', hue='training_mode', ax=axs[2])
+    sns.stripplot(data=all_results_df, x='model_id', y='test_elapsed_time', hue='training_mode', ax=axs[2], size=10)
     axs[2].set_ylabel('Test Elapsed time (in seconds)')
 
-    fig.text(0.25, 0.90, 'MCC', ha='center', fontsize=24)
-    fig.text(0.5, 0.90, 'AUPRC', ha='center', fontsize=24)
-    fig.text(0.75, 0.90, 'Elapsed time (in seconds)', ha='center', fontsize=24)
+    axs[0].set_title('MCC', fontsize=30)
+    axs[1].set_title('AUPRC', fontsize=30)
+    axs[2].set_title('Elapsed time (in seconds)', fontsize=30)
+
+    # fig.text(0.2, 0.90, 'MCC', ha='center', fontsize=24)
+    # fig.text(0.5, 0.90, 'AUPRC', ha='center', fontsize=24)
+    # fig.text(0.8, 0.90, 'Elapsed time (in seconds)', ha='center', fontsize=24)
 
     for i in [0, 1, 2]:
         axs[i].tick_params(axis='x', rotation=45)
 
     plt.subplots_adjust(hspace=0.6)
+    plt.tight_layout()
 
     if ADD_BASELINES_TO_FIG:
-        plt.savefig(Path(FINAL_RESULTS) / 'results_with_hlines_for_paper.png')
+        plt.savefig(Path(FINAL_RESULTS, dpi=600) / 'results_with_hlines_for_paper.png')
     else:
-        plt.savefig(Path(FINAL_RESULTS) / 'results_for_paper.png')
+        plt.savefig(Path(FINAL_RESULTS, dpi=600) / 'results_for_paper.png')
 
 
 def main():
